8b300c8770c648a5bf0ff2b4952db4e07d86525d
Jenish <jrpatel47@myseneca.ca>
2022-03-25 20:50:17 -0400

    Initial commit

---
+++ js/style.js
@@ -0,0 +1,15 @@
+
+// Name    : Jenish Rajnikant Patel
+// ID      : 131733206
+// Email   : jrpatel47@myseneca.ca
+// Section : NCC -->
+
+function hide(){
+    document.getElementById("secret").innerHTML="<input type='hidden'>"; 
+};
+
+
+function opentext(){
+    document.getElementById("secret").innerHTML="<br><br><input size='30' placeholder='Enter text: ' style='height:30px;' type='number' name='secretText'>";
+};
+
